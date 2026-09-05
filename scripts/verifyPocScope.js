ad8f59136a8eec8e361f694aa5b2ba5d45bbf079
Muthurajan P. <1404@smartcliff.loc>
2026-09-05 10:10:21 +0530

    first commit

--- scripts/verifyPocScope.js
+++ scripts/verifyPocScope.js
@@ -24,7 +24,7 @@ const tokenModal = require("../models/tokenModal");
 const ClientManagement = require("../models/ClientManagementModel");
 const CourseStructure = require("../models/Courses/courseStructureModal");
 
-const BASE = process.env.VERIFY_BASE_URL || "https://http://localhost:5533";
+const BASE = process.env.VERIFY_BASE_URL || "https://https://lmsserver-yeve.onrender.com";
 const POC_EMAIL = process.env.VERIFY_POC_EMAIL || "poc@gmail.com";
 
 let pass = 0;
